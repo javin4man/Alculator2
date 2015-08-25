99479a8512ded5c063819b83540d9a415b498f8f
Javin Foreman <javin4man@gmail.com>
2015-08-25 15:00:20 -0400

    added background, age button

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,25 +1,9 @@
 // jshint devel:true
 //console.log('\'Allo \'Allo!');//
 
-<title>Alculator - Javascript</title>
+function calc() {
 
-form id="form _data" action="main.js" method="post">
+ var num1, num2;
 
-<input type="text" id="num1" />
-
- <select id="sign">
- <option value="+">X</option>
-
-<input type="text" id="num2" />
-
-<input type="button"  value="Calculate" id="btn" />
-
-<br>
-<br>
-
-Result:
-<input type="text" id="result" />
-
-</fieldset>
-
-</form>
+ // var sign = "="
+}
