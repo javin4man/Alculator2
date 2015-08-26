536df773e5ac3ebc5e26c5a2016bc344e39412a2
Javin Foreman <javin4man@gmail.com>
2015-08-26 16:20:40 -0400

    yo yo yo

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,13 @@
+'use strict';
+
 // // jshint devel:true
 // //console.log('\'Allo \'Allo!');//
 
 // var age = ["21-25", "26-30", "31-35", "35+"];
 
-var height = [4'8" to 5'2", 5'3 to 5'7", 5'8 to 6'2", 6'3 to 6'8"];
+// var height = [4 foot 8 inches to 5 foot 2 inches, 5 foot 3 inches to 5 foot 7 inches, 5 foot 8 inches to 6 foot 2 inches, 6 foot 3 inches to 6 foot 8 inches];
 
-var weight = [95 lbs to 125 lbs, 126 lbs to 156 lbs,
+// var weight = [95 lbs to 125 lbs, 126 lbs to 156 lbs,
 // 157 lbs to 187 lbs,  188 lbs to 218 lbs, 219 lbs to 250 + lbs];
 
 //if age[0 or 1] && height [0 or 1] && weight[0 or 1]
@@ -16,17 +18,28 @@ var weight = [95 lbs to 125 lbs, 126 lbs to 156 lbs,
 
 // if age[any] && height [2 or 3] && weight[4 or 5]
 //   then drinks = 4;
+$('#submit').click(function() {
 
-var chosenAge = $('#age').val();
-if(chosenAge < 2 && chosenWeight < 2){
-  $('#result').val('2 drinks');
-}
+  var chosenAge = $('#age').val();
+  var chosenHeight = $('#height').val();
+  var chosenWeight = $('#weight').val();
 
-var chosenAge = $('#age').val();
-if(chosenAge === 2, 3  && chosenWeight ===2, 3 && chosenHeight ===2, 3 ){
-  $('#result').val('3 drinks');
-}
+  console.log('Age: ' + chosenAge + ' Height: ' + chosenHeight + ' Weight: ' + chosenWeight);
+  if(chosenAge < 2 && chosenHeight < 2 && chosenWeight < 2){
+    $('#result').val('2 drinks');
+    console.log('we made it');
+  }
+  else if(chosenAge > 0 && (chosenWeight === 2 || chosenWeight === 3 ) && chosenHeight > 2 ){
+    $('#result').val('3 drinks');
 
-var chosenAge = $('#age').val();
-if(chosenAge < 0  && chosenWeight ===2, 3 && chosenHeight ===4, 5 ){
-  $('#result').val('3 drinks');
+  }
+  else if(chosenAge > -1 && chosenWeight > 3 && chosenHeight > 1 ){
+   $('#result').val('4 drinks');
+  }
+
+  else {
+    $('#result').val('should you be drinking?');
+  }
+   console.log('Age: ' + chosenAge + ' Height: ' + chosenHeight + ' Weight: ' + chosenWeight);
+
+});
