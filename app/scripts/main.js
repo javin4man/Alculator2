db9f69f2112040dd0c7ad97e485e33f0b3d4ee87
Javin Foreman <javin4man@gmail.com>
2015-08-25 20:48:45 -0400

    added logo, fixed arrays.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,5 +5,5 @@ var age = ["21-25", "26-30", "31-35", "35+"];
 
 var height = [4'8" to 5'2", 5'3 to 5'7", 5'8 to 6'2", 6'3 to 6'8"];
 
-var weight = [95 lbs to 125 lbs, 126 lbs to 156 lbs, 157 lbs to 187 lbs
-157 lbs to 188 lbs, 189 lbs to 219 lbs, 220 lbs to 250 + lbs];
+var weight = [95 lbs to 125 lbs, 126 lbs to 156 lbs,
+157 lbs to 187 lbs,  188 lbs to 218 lbs, 219 lbs to 250 + lbs];
